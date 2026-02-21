2da4d554a0677f76ce575a51b7c43807d07bf62e
Andr3xDev <andr3xdev@gmail.com>
2026-02-21 12:25:25 -0500

    feat: add logic to yellow step

--- tdd-lab/src/main/java/escuelaing/arcn/tdd/FizzBuzz.java
+++ tdd-lab/src/main/java/escuelaing/arcn/tdd/FizzBuzz.java
@@ -6,6 +6,15 @@ package escuelaing.arcn.tdd;
 public class FizzBuzz {
 
     public static String fizzbuzz(int n) {
-        return String.valueOf(n);
+        if (n % 15 == 0) {
+            return "FizzBuzz";
+        } else if (n % 3 == 0) {
+            return "Fizz";
+        } else if (n % 5 == 0) {
+            return "Buzz";
+        } else {
+            return String.valueOf(n);
+        }
     }
+
 }
